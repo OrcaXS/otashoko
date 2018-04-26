8255f1004226dd37f588a7cb73376f02c480997b
OrcaXS Suen <sun16281394@gmail.com>
2018-04-26 23:32:43 +0800

    Initial commit

---
+++ client/.eslintrc.js
@@ -0,0 +1,68 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'jsdoc',
+  ],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: 'webpack.config.js',
+    },
+  },
+  // add your custom rules here
+  'rules': {
+    'max-len': 'warn',
+    'no-unused-vars': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+        'state',
+        'el',
+        'rootState', // for vuex
+        'app', // for plugin
+      ]
+    }],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'import/prefer-default-export': 'warn',
+    'semi': ['error', 'always', { 'omitLastInOneLineBlock': true }],
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    // jsdoc
+    'jsdoc/check-param-names': 1,
+    'jsdoc/check-tag-names': 1,
+    'jsdoc/check-types': 1,
+    'jsdoc/newline-after-description': 1,
+    'jsdoc/require-description-complete-sentence': 1,
+    'jsdoc/require-hyphen-before-param-description': 1,
+    'jsdoc/require-param': 1,
+    'jsdoc/require-param-description': 1,
+    'jsdoc/require-param-name': 1,
+    'jsdoc/require-param-type': 1,
+    'jsdoc/require-returns-description': 1,
+    'jsdoc/require-returns-type': 1,
+  }
+}
